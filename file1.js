9a50ebbc2ef88df2d4772bfc985a408e64fab14f
khawahish <khawahisharora20@gmail.com>
2019-03-16 18:55:54 +0530

    changes in the greeter branch

--- file1.js
+++ file1.js
@@ -5,3 +5,12 @@ console.log('line2')
 console.log('line3')
 console.log('line 4')
 console.log('line 5')
+
+function greeter(name)
+{
+    console.log('how are you?  ')
+    console.log('Hope this session is useful for you '+name)
+   
+}
+
+greeter('khawahish')
